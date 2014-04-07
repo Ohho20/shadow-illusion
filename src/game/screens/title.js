bb38fc096d7027137fbdca7a4fb15dfbe0e256c5
Bruce Coddington <brucecoddington@gmail.com>
2014-04-07 18:47:11 -0500

    initial commit of demo game

---
+++ src/game/screens/title.js
@@ -0,0 +1,38 @@
+game.TitleScreen = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		
+		// title screen
+		me.game.world.addChild(
+			new me.SpriteObject(
+				0,0,me.loader.getImage('title_screen')
+			),
+			1
+		);
+
+		// renderable component with scrolling text
+		me.game.world.addChild( new (me.Renderable.extend({
+
+			init : {},
+			scrollover: {},
+			update: {}, 
+			draw: function (context) {
+
+			},
+			onDestroyEvent: function () {
+				// just in case
+				this.scrollertween.stop();
+			}
+		})), 2);
+	},
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		; // TODO
+	}
+});
--- src/main.js
+++ src/main.js
@@ -23,6 +23,29 @@ angular.module('main', [
   // putting state into $rootScope so that these services are available in views
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
-  
+
+  game.onload();
+
+  // Mobile browser hacks
+  if (me.device.isMobile && !navigator.isCocoonJS) {
+    // Prevent the webview from moving on a swipe
+    window.document.addEventListener("touchmove", function (e) {
+      e.preventDefault();
+      window.scroll(0, 0);
+      return false;
+    }, false);
+
+    // Scroll away mobile GUI
+    (function () {
+      window.scrollTo(0, 1);
+      me.video.onresize(null);
+    }).defer();
+
+    me.event.subscribe(me.event.WINDOW_ONRESIZE, function (e) {
+      window.scrollTo(0, 1);
+    });
+  }
+
+
   $log.info("Application running.");
 });
