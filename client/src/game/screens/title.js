d4177479a0cb9626922409ac5cc5be1553441597
Bruce Coddington <brucecoddington@gmail.com>
2014-04-08 18:54:27 -0500

    Using solved demo code for the demo game.

--- client/src/game/screens/title.js
+++ client/src/game/screens/title.js
@@ -17,19 +17,56 @@ angular.module('game.screens.title', [])
 
         // renderable component with scrolling text
         var ScrollingText = me.Renderable.extend({
-          init : {},
-          scrollover: {},
-          update: {}, 
-          draw: function (context) {
-
+          init : function() {
+            this.parent(new me.Vector2d(0, 0), me.game.viewport.width, me.game.viewport.height);
+            // font for the scrolling text
+            this.font = new me.BitmapFont("32x32_font", 32);
+            
+             // a tween to animate the arrow
+            this.scrollertween = new me.Tween(this)
+              .to({scrollerpos: -2200 }, 10000)
+              .onComplete(this.scrollover.bind(this))
+              .start();
+    
+            this.scroller = "A SMALL STEP BY STEP TUTORIAL FOR GAME CREATION WITH MELONJS       ";
+            this.scrollerpos = 600;
           },
-          onDestroyEvent: function () {
-            // just in case
+          
+          scrollover: function() {
+            // reset to default value
+            this.scrollerpos = 640;
+            this.scrollertween.to({scrollerpos: -2200 }, 10000).onComplete(this.scrollover.bind(this)).start();
+          },
+        
+          update : function (dt) {
+            return true;
+          },
+          
+          draw : function (context) {
+            this.font.draw (context, "PRESS ENTER TO PLAY", 20, 240);
+            this.font.draw(context, this.scroller, this.scrollerpos, 440);
+          },
+
+          onDestroyEvent : function() {
+            //just in case
             this.scrollertween.stop();
           }
         });
 
         me.game.world.addChild( new ScrollingText(), 2);
+
+        // change to play state on press Enter or click/tap
+        me.input.bindKey(me.input.KEY.ENTER, "enter", true);
+        me.input.bindPointer(me.input.mouse.LEFT, me.input.KEY.ENTER);
+        
+        this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
+          if (action === "enter") {
+            // play something on tap / enter
+            // this will unlock audio on mobile devices
+            me.audio.play("cling");
+            me.state.change(me.state.PLAY);
+          }
+        });
       },
       
       
@@ -37,7 +74,9 @@ angular.module('game.screens.title', [])
        *  action to perform when leaving this screen (state change)
        */
       onDestroyEvent: function() {
-        // TODO
+        me.input.unbindKey(me.input.KEY.ENTER);
+        me.input.unbindPointer(me.input.mouse.LEFT);
+        me.event.unsubscribe(this.handler);
       }
     });
   });
