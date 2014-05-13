ab958093da022bd6bc59e13eb219e55397bf84d1
Bruce Coddington <brucecoddington@gmail.com>
2014-05-13 18:48:07 -0500

    added the death screen

---
+++ client/src/game/screens/death.js
@@ -0,0 +1,85 @@
+angular.module('game.screens.death', [])
+
+.factory('DeathScreen', function (ScrollingDeathText) {
+    
+    return me.ScreenObject.extend({
+      /** 
+       *  action to perform on state change
+       */
+      onResetEvent: function() {  
+        
+        // title screen
+        me.game.world.addChild(
+          new me.SpriteObject(
+            0,0,me.loader.getImage('cave')
+          ),
+          1
+        );
+
+        // renderable component with scrolling text
+        me.game.world.addChild( new ScrollingDeathText(), 2);
+
+        // change to play state on press Enter or click/tap
+        me.input.bindKey(me.input.KEY.ENTER, "enter", true);
+        me.input.bindPointer(me.input.mouse.LEFT, me.input.KEY.ENTER);
+        
+        this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
+          if (action === "enter") {
+            // play something on tap / enter
+            // this will unlock audio on mobile devices
+            me.audio.play("cling");
+            me.state.change(me.state.PLAY);
+          }
+        });
+      },
+      
+      /** 
+       *  action to perform when leaving this screen (state change)
+       */
+      onDestroyEvent: function() {
+        me.input.unbindKey(me.input.KEY.ENTER);
+        me.input.unbindPointer(me.input.mouse.LEFT);
+        me.event.unsubscribe(this.handler);
+      }
+    });
+  })
+
+  .factory('ScrollingDeathText', function () {
+    return me.Renderable.extend({
+      init : function() {
+        this.parent(new me.Vector2d(0, 0), me.game.viewport.width, me.game.viewport.height);
+        // font for the scrolling text
+        this.font = new me.BitmapFont("32x32_font", 32);
+        
+         // a tween to animate the arrow
+        this.scrollertween = new me.Tween(this)
+          .to({scrollerpos: -2200 }, 10000)
+          .onComplete(this.scrollover.bind(this))
+          .start();
+
+        this.scroller = "NICE TRY, GIVE IT ANOTHER SHOT.         I BET YOU CAN DO IT THIS TIME!       ";
+        this.scrollerpos = 600;
+      },
+      
+      scrollover: function() {
+        // reset to default value
+        this.scrollerpos = 640;
+        this.scrollertween.to({scrollerpos: -2200 }, 10000).onComplete(this.scrollover.bind(this)).start();
+      },
+    
+      update : function (dt) {
+        return true;
+      },
+      
+      draw : function (context) {
+        this.font.draw(context, "YOU DIED!!", 128, 64);
+        this.font.draw (context, "PRESS ENTER TO PLAY", 12, 240);
+        this.font.draw(context, this.scroller, this.scrollerpos, 440);
+      },
+
+      onDestroyEvent : function() {
+        //just in case
+        this.scrollertween.stop();
+      }
+    });
+  });
